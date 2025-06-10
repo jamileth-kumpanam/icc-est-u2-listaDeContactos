9660c5975f59dc25a609def3ce4b8402250d15e4
jamileth-kumpanam <jamilethkumpanam@MacBook-Air-de-Jamileth.local>
2025-06-10 07:45:43 -0500

    second commit

--- src/Model/LinkedList.java
+++ src/Model/LinkedList.java
@@ -1,5 +1,39 @@
 package Model;
 
 public class LinkedList {
+
+    public Node head;
+    private int size;
+
+    public LinkedList(Node head, int size) {
+        this.head = head;
+        this.size = size;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public void setHead(Node head) {
+        this.head = head;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    
+
+    public void appendToTail(int value){
+        if (head == null){
+            head = new Node (value);
+            size++;
+            return;
+        }
+    }
     
 }
